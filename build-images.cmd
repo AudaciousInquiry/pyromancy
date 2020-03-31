c2b6de18eebf05b07383d02fbbf2ceb723787e2e
Keith W. Boone <36998841+keithboone@users.noreply.github.com>
2020-03-31 01:07:04 -0400

    Fix jar file location

--- build-images.cmd
+++ build-images.cmd
@@ -1,1 +1,1 @@
-%JAVAEXE% -classpath %PLANTUMLCP% -jar %PLANTUMLCP%\plantuml.jar -tsvg %LOC%\images-source -o ..\images
+%JAVAEXE% -classpath %PLANTUMLCP% -jar %PLANTUMLCP% -tsvg %LOC%\images-source -o ..\images
